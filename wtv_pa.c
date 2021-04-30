41e10f8bb9f665c72e03a1d9ec9c32271ba3c3fd
Jay Sorg <jay.sorg@gmail.com>
2021-04-30 09:50:45 -0700

    update copyright year

--- wtv_pa.c
+++ wtv_pa.c
@@ -1,7 +1,7 @@
 /**
  * pulseaudio calls
  *
- * Copyright 2015-2018 Jay Sorg <jay.sorg@gmail.com>
+ * Copyright 2015-2021 Jay Sorg <jay.sorg@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
